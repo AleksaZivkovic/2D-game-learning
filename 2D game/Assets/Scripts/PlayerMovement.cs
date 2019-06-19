7f9e99f516c71604dda673a64dbe1c26b854f491
AleksaZivkovic <leksafx@gmail.com>
2019-06-19 20:35:15 +0200

    Added game
    Added my 2D game with jump PlayerMovement script(only jump so far) and animations.

---
+++ 2D game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour{
+    Rigidbody2D rb;
+    bool isAbleToJump = true;
+
+    public Animator animator;
+    public float jumpSpeed = 100f;
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            if(isAbleToJump) {
+                animator.SetBool("readyForJump", true);
+            }
+        }
+
+        if(Input.GetKeyUp(KeyCode.Space)) {
+            if(isAbleToJump) {
+                animator.SetBool("isJumping", true);
+                rb.AddForce(new Vector2(0, jumpSpeed));
+                isAbleToJump = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log(collision.collider.tag);
+        if(collision.collider.tag == "ground") {
+            isAbleToJump = true;
+            animator.SetBool("readyForJump", false);
+            animator.SetBool("isJumping", false);
+        }
+    }
+}
